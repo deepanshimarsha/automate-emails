f2ff2f0cbde768c560818ac70e833dd9215108fb
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2022-09-19 10:44:18 +0530

    configured heroku

--- src/config/settings.py
+++ src/config/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #DataFlair
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- src/config/urls.py
+++ src/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('subscribe/', include('subscribe.urls')),
+    path('', include('subscribe.urls')),
 ]
